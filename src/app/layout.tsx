3e5b8185f15cef2715601387045278aec0370a02
Jargaltogtokh <Jagaa155@gmail.com>
2024-12-24 18:15:28 +0800

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Footer } from "./_Components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="min-h-screen bg-background text-foreground">
+          {" "}
+          {children}{" "}
+        </main>
+        <Footer />
       </body>
     </html>
   );
