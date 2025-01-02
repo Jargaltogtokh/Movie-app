bec2f3e7ba6b1ead0cd6de39f07ba2678db02c40
Jargaltogtokh <jagaa155@gmail.com>
2025-01-02 22:02:38 +0800

    updated

---
+++ src/app/_Components/SearchByName.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { options } from "@/constants/api"
+import { useEffect, useState } from "react"
+
+export const SearchByName = () => {
+    const [search, setSearch] = useState("");
+    const [movie, setMovie] = useState(null);
+    const [isclient, setIsClient]= useState(false);
+    
+    const onChangePage = (event) => {
+        setSearch(event.target.value);
+      };
+
+    const onPressEnter = (e) => {
+        if (e.code === "Enter") {setMovie(search);}
+    };
+
+    useEffect (() => {setIsClient (true)}, []); 
+
+    useEffect (()=> {fetch(`https://api.themoviedb.org/3/movie/`,
+            options
+          )
+          .then((response) => response.json())
+          .then((data) => {
+      console.log(data);
+    return (  
+        <div>
+        <input
+        type="text"
+        value={search}
+        onChange={onChangePage}
+        onKeyDown={onPressEnter}
+        placeholder="Search for a movie"
+        />
+        {movie && <div> Searching for: {movie} </div>}
+        </div>
+        );
+};
