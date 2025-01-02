ad9e1a10cd482b3e90d2602a78cd5e3a8787aad8
Jargaltogtokh <jagaa155@gmail.com>
2025-01-02 22:08:37 +0800

    updated

--- src/app/_Components/Header.tsx
+++ src/app/_Components/Header.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 import { FilterGenre } from "./FilterGenre";
+import { SearchByName } from "./SearchByName";
 
 export const Header = () => {
   return (
     <div className="flex mt-0 justify-between p-6">
       <div className="flex gap-5">
-        <Image width={5} height={5} src="/film_2.png" alt="Logo" />
+        <img src="film_2.png" className="w-[20px] h-[20px]"/>
         <p className="italic font-bold mb-[10px] text-blue-600">Movie Z</p>
       </div>
-      <div className="flex justify-center items-center">
+      <div className="flex justify-end items-center">
         <FilterGenre />
       </div>
       <div className="flex gap-5">
@@ -17,6 +18,7 @@ export const Header = () => {
           className="w-[20px] h-[20px] border-2 rounded-md"
           alt="Search"
         />
+        <SearchByName/>
         <img
           src="moon.png"
           className="w-[20px] h-[20px] border-2 rounded-md"
